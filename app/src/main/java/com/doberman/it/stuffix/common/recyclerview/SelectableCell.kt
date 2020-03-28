b493af04db27d8463f67463708562b9cc1ee78d2
Dawiar <daviar@bigmir.net>
2020-03-28 07:13:02 +0300

    setup selectable cell

--- app/src/main/java/com/doberman/it/stuffix/common/recyclerview/SelectableCell.kt
+++ app/src/main/java/com/doberman/it/stuffix/common/recyclerview/SelectableCell.kt
@@ -1,18 +1,21 @@
 package com.doberman.it.stuffix.common.recyclerview
 
-import androidx.databinding.ViewDataBinding
 import androidx.lifecycle.LifecycleOwner
-import com.doberman.it.stuffix.BR
 import com.doberman.it.stuffix.R
+import com.doberman.it.stuffix.databinding.FragmentLocationsItemBinding
 
-class SelectableCell(val viewModel: SelectableSubViewModel): BindableCell<ViewDataBinding> {
+class SelectableCell(val viewModel: SelectableSubViewModel) :
+    BindableCell<FragmentLocationsItemBinding> {
     override val layoutId: Int
         get() = R.layout.fragment_locations_item
 
-    override fun bind(binding: ViewDataBinding, lifecycleOwner: LifecycleOwner) {
-        binding.setVariable(BR.viewModel, viewModel )
+    override fun bind(binding: FragmentLocationsItemBinding, lifecycleOwner: LifecycleOwner) {
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = lifecycleOwner
     }
 
-    override fun unbind(binding: ViewDataBinding) = Unit
+    override fun unbind(binding: FragmentLocationsItemBinding) {
+        binding.unbind()
+    }
 
 }
